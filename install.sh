fbbd4496f0c258986ab1ff4f10e8a9ea434af13d
wiktorj <wiktor.jurasz@gmail.com>
2020-05-06 13:54:11 +0200

    added script

---
+++ install.sh
@@ -0,0 +1,7 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install git
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install tmux
+brew install fzf
+brew install autojump
+brew install pyenv
